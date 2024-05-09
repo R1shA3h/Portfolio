fd461eff8eca789b5ff2194839a7cd7fed7d5d9d
Rishabh <r1sha3hjain@gmail.com>
2024-05-10 00:47:36 +0530

    Added react mouse follower

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import Navbar from './Components/Nav Bar/Navbar'
-import Hero from './Components/Hero/Hero'
-import About from './Components/About/About'
-import Services from './Components/Services/Services'
-import MyWork from './Components/MyWork/MyWork'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
+import Navbar from "./Components/Nav Bar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
+import Services from "./Components/Services/Services";
+import MyWork from "./Components/MyWork/MyWork";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import { MouseFollower, UpdateFollower } from "react-mouse-follower";
+
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Services/>
-      <MyWork/>
-      <Contact/>
-      <Footer/>
+      <MouseFollower />
+      <UpdateFollower mouseOptions={{
+        backgroundColor: "white",
+      }}>
+        <Navbar />
+        <Hero />
+        <About />
+        <Services />
+        <MyWork />
+        <Contact />
+        <Footer />
+      </UpdateFollower>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import './Hero.css'
-import profile_img from '../../assets/myphoto.jpg'
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { UpdateFollower } from "react-mouse-follower";
+import "./Hero.css";
 const Hero = () => {
+  const handleClick = () => {
+    window.open(
+      "https://drive.google.com/file/d/1VQmKbJ2tCNKzv3hFgdQbic9ISE1TQiWK/view?usp=sharing",
+      "_blank"
+    ); // Change the URL to the link you want to open
+  };
   return (
-    <div id='home' className='hero'>
-        <img src={profile_img} alt=''/>
-        <h1><span>I am Rishabh Jain,</span> Frontend Developer based in India</h1>
-        <p>I am a prefinal year student of computer science and engineering from Ramdeobaba College and Management.</p>
-        <div className="hero-action">
-            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-            <div className="hero-resume">My Resume</div>
-        </div>
-    </div>
-  )
-}
+    <div id="home" className="hero">
+      <UpdateFollower
+       style={{
+        display: "flex",
+        justifyContent: "center"
+       }}
+        mouseOptions={{
+          zIndex: 10,
+          mixBlendMode: 'plus-lighter',
+          backgroundColor: "black",
+          rotate: 360,
+          backgroundElement: (
+            <div className="video-container">
+              <video src="/video.mp4" autoPlay muted loop></video>
+            </div>
+          ),
+          scale: 20,
+        }}
+      >
+        <h1>
+          <span>I am Rishabh Jain,</span> Frontend Developer based in India
+        </h1>
+      </UpdateFollower>
 
-export default Hero
+      <p>
+        I am a prefinal year student of computer science and engineering from
+        Ramdeobaba College and Management.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
+        <div className="hero-resume" onClick={handleClick}>
+          My Resume
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
